9494fa2d2d5f4020b1068e2dac907cbc807c1fbb
Bayu Nugroho <bayu404.dart@gmail.com>
2023-08-23 15:45:13 +0700

    feat: add search activity by path

--- lib/presentation/dashboard/page/dashboard_page.dart
+++ lib/presentation/dashboard/page/dashboard_page.dart
@@ -15,44 +15,84 @@ class DashboardPage extends StatelessWidget {
       child: Consumer<DashboardNotifier>(
         builder: (context, provider, child) {
           return Scaffold(
-            floatingActionButton: FloatingActionButton(
-              onPressed: () {
-                provider.clearAllResponses();
-              },
-              backgroundColor: Colors.purple.withOpacity(0.6),
-              child: const Icon(Icons.delete),
-            ),
-            appBar: AppBar(
-              actions: [
-                PopupMenuButton(itemBuilder: (context) {
-                  return [
-                    const PopupMenuItem(
-                      value: SortActivity.byTime,
-                      child: Text('Time'),
-                    ),
-                    const PopupMenuItem(
-                      value: SortActivity.byMethod,
-                      child: Text('Method'),
-                    ),
-                    const PopupMenuItem(
-                      value: SortActivity.byStatus,
-                      child: Text('Status'),
-                    ),
-                  ];
-                }, onSelected: (value) {
-                  provider.sortAllResponses(value);
-                })
-              ],
-              title: const Text('Http Activities'),
-              backgroundColor: Colors.purple.withOpacity(0.6),
-            ),
-            body: buildBody(context, provider),
-          );
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {
+                  provider.clearAllResponses();
+                },
+                backgroundColor: Colors.purple.withOpacity(0.6),
+                child: const Icon(Icons.delete),
+              ),
+              appBar: AppBar(
+                leading: IconButton(
+                  onPressed: () {
+                    if (provider.isSearch) {
+                      provider.toggleSearch();
+                      return;
+                    }
+
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(Icons.arrow_back),
+                ),
+                actions: [
+                  IconButton(
+                      onPressed: () {
+                        provider.toggleSearch();
+                      },
+                      icon:
+                          Icon(provider.isSearch ? Icons.close : Icons.search)),
+                  PopupMenuButton(itemBuilder: (context) {
+                    return [
+                      const PopupMenuItem(
+                        value: SortActivity.byTime,
+                        child: Text('Time'),
+                      ),
+                      const PopupMenuItem(
+                        value: SortActivity.byMethod,
+                        child: Text('Method'),
+                      ),
+                      const PopupMenuItem(
+                        value: SortActivity.byStatus,
+                        child: Text('Status'),
+                      ),
+                    ];
+                  }, onSelected: (value) {
+                    provider.sortAllResponses(value);
+                  })
+                ],
+                title: !provider.isSearch
+                    ? const Text('Http Activities')
+                    : TextField(
+                        style: const TextStyle(color: Colors.white),
+                        autofocus: true,
+                        onChanged: (value) {
+                          provider.search(value);
+                        },
+                        focusNode: provider.focusNode,
+                        controller: provider.searchController,
+                        decoration: const InputDecoration(
+                          hintText: 'Search',
+                          hintStyle: TextStyle(color: Colors.white),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color:
+                                    Colors.white), // Set focused border color
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color:
+                                    Colors.white), // Set enabled border color
+                          ),
+                        ),
+                      ),
+                backgroundColor: Colors.purple.withOpacity(0.6),
+              ),
+              body: buildBody(context, provider));
         },
       ),
     );
   }
-
+  
   Widget buildBody(BuildContext context, DashboardNotifier provider) {
     if (provider.getAllResponsesState == RequestState.loading) {
       return const Center(
@@ -64,9 +104,13 @@ class DashboardPage extends StatelessWidget {
       );
     } else {
       return ListView.builder(
-        itemCount: provider.getAllResponses.length,
+        itemCount: provider.isSearch
+            ? provider.activityFromSearch.length
+            : provider.getAllResponses.length,
         itemBuilder: (context, index) {
-          var data = provider.getAllResponses[index];
+          var data = provider.isSearch
+              ? provider.activityFromSearch[index]
+              : provider.getAllResponses[index];
           return InkWell(
             onTap: () {
               Navigator.push<void>(
